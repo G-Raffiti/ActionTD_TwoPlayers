a66442d99e856df031fd5144c0574c33bcdc24b4
Raphael Bonneval <grugroo128@gmail.com>
2024-06-22 20:58:36 +0200

    ADD Projectile sync

--- Scripts/Globals/Guid.gd
+++ Scripts/Globals/Guid.gd
@@ -3,5 +3,5 @@ extends Node
 var id_gen: int = 1
 
 func get_id() -> String:
-    id_gen += 1
-    return str(id_gen)
+	id_gen += 1
+	return str(id_gen)
--- Scripts/TD/Towers/projectile.gd
+++ Scripts/TD/Towers/projectile.gd
@@ -11,22 +11,26 @@ var old_targets: Array[StringName] = []
 var direction: Vector3
 
 func initialize(in_stats: TowerStats.ProjectileStats, in_target: Node3D, in_direction: Vector3) -> void:
+	if not multiplayer.is_server(): return
 	stats = in_stats
 	change_target(in_target)
 	$ExplosionArea/ExplosionCollisionShape.shape.radius = stats.explosion_radius
 	direction = in_direction
 
 func change_target(in_target: Node3D):
+	if not multiplayer.is_server(): return
 	target = in_target
 	target.tree_exiting.connect(_on_target_destroyed)
 	old_targets.append(target.name)
 
 func start():
+	if not multiplayer.is_server(): return
 	get_tree().create_timer(stats.lifetime).timeout.connect(_lifetime_expire)
 	set_physics_process(true)
 	body_entered.connect(_body_entered)
 
 func _physics_process(delta: float) -> void:
+	if not multiplayer.is_server(): return
 	if stats.homing and target:
 		direction = global_position.direction_to(target.global_position)
 
@@ -34,6 +38,7 @@ func _physics_process(delta: float) -> void:
 	look_at(global_position + direction, Vector3.ONE)
 
 func _on_new_target_found(projectile_name: StringName, new_target: Node3D) -> void:
+	if not multiplayer.is_server(): return
 	if name != projectile_name:
 		return
 	SignalBus.on_nearest_target_found.disconnect(_on_new_target_found)
@@ -50,16 +55,19 @@ func _on_new_target_found(projectile_name: StringName, new_target: Node3D) -> vo
 	direction = global_position.direction_to(target.global_position)
 
 func _body_entered(body: Node3D):
+	if not multiplayer.is_server(): return
 	if 'implements' in body and body.implements.has(I.Killable):
 		if stats.explosion_radius <= 0:
 			body.take_damage(stats.damage)
 		explode()
 
 func _lifetime_expire():
+	if not multiplayer.is_server(): return
 	explode()
 	queue_free()
 
 func explode():
+	if not multiplayer.is_server(): return
 	if stats.explosion_radius > 0:
 		for node in explosion_area.get_overlapping_bodies():
 			if 'implements' in node and node.implements.has(I.Killable):
@@ -76,6 +84,7 @@ func explode():
 		find_new_target()
 
 func find_new_target():
+	if not multiplayer.is_server(): return
 	if is_instance_valid(target):
 		target.tree_exiting.disconnect(_on_target_destroyed)
 	target = null
@@ -84,6 +93,7 @@ func find_new_target():
 		SignalBus.on_projectile_search_target.emit(name, old_targets, position)
 
 func _on_target_destroyed() -> void:
+	if not multiplayer.is_server(): return
 	target = null
 	if stats.homing:
 		find_new_target()
